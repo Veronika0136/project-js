35712e780548e23297b26b4de070a732debe45c7
Veronika0136 <Artemova331@gmail.com>
2025-03-08 19:19:09 +0200

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -1,4 +1,5 @@
 new Swiper(".swiper", {
+  slidesPerView: "auto",
   slidesPerView: 4,
   spaceBetween: 16,
   slidesPerGroup: 1,
@@ -28,6 +29,46 @@ new Swiper(".swiper", {
     },
     1440: {
       slidesPerView: 4,
+      spaceBetween: 10,
     },
   },
 });
+
+const refs = {
+  list: document.querySelector(".reviews-list"),
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const result = await getReviews();
+    const markup = reviewsTemplate(result);
+    refs.list.innerHTML = markup;
+  } catch (error) {
+    refs.list.innerHTML = `<p class="reviews-err">---Not found---</p>`;
+    console.log(error);
+  }
+});
+
+async function getReviews() {
+  const baseURL = "https://portfolio-js.b.goit.study/api/reviews";
+  const result = await axios.get(baseURL);
+  return result.data;
+}
+
+function reviewTemplate(item) {
+  const { author, avatar_url, review } = item;
+
+  return `<li class="reviews-item swiper-slide">
+            <img
+              class="reviews-img"
+              src="${avatar_url}"
+              alt="foto author"
+            />
+            <h2 class="reviews-name">${author}</h2>
+            <p class="reviews-txt">${review}</p>
+          </li>`;
+}
+
+function reviewsTemplate(arr) {
+  return arr.map(reviewTemplate).join("");
+}
